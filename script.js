6956ae03c8a1c9cbd4e8c406a68161638e1ae7af
Hayat Azeem <hayatazeem5oct2004@gmail.com>
2025-06-27 01:04:02 +0530

    Final commit

--- script.js
+++ script.js
@@ -160,6 +160,44 @@ function flagAnimation(){
         })
 })
 }
+function footerAnimation() {
+
+  var clutter = ""
+  var clutter2 = ""
+  document.querySelector("#footer h1").textContent.split("").forEach(function (elem) {
+    clutter += `<span>${elem}</span>`
+  })
+  document.querySelector("#footer h1").innerHTML = clutter
+  document.querySelector("#footer h2").textContent.split("").forEach(function (elem) {
+    clutter2 += `<span>${elem}</span>`
+  })
+  document.querySelector("#footer h2").innerHTML = clutter2
+
+
+  document.querySelector("#footer-text").addEventListener("mouseenter", function () {
+    gsap.to("#footer h1 span", {
+      opacity: 0,
+      stagger: 0.05
+    })
+    gsap.to("#footer h2 span", {
+      delay: 0.35,
+      opacity: 1,
+      stagger: 0.1
+    })
+  })
+  document.querySelector("#footer-text").addEventListener("mouseleave", function () {
+    gsap.to("#footer h1 span", {
+      opacity: 1,
+      stagger: 0.1,
+      delay: 0.35,
+
+    })
+    gsap.to("#footer h2 span", {
+      opacity: 0,
+      stagger: 0.05
+    })
+  })
+}
 
 // var tanimate = document.querySelectorAll("#textanimate");
 // tanimate.addEventListener("mouseenter",function(){
@@ -172,3 +210,4 @@ cursorAnimation()
 locomotive()
 sheryAnimation()
 flagAnimation()
+footerAnimation()
